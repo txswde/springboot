b2ff56f6948b5275ac2f50d6f1a851e998de7eee
ng099 <14260297+ng099@user.noreply.gitee.com>
2025-07-24 15:09:44 +0800

    创建项目

---
+++ mat-mall/src/main/java/com/example/matmall/MatMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.matmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatMallApplication.class, args);
+    }
+
+}
---
+++ mat-mall/src/test/java/com/example/matmall/MatMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
